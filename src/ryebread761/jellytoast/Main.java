1095a82aca5c247d77272576e6112c9cd204d881
rye761 <ryebread761@gmail.com>
2014-01-29 14:46:04 -0600

    New method works, fixes old bug (some toasts showing KK style)

--- src/ryebread761/jellytoast/Main.java
+++ src/ryebread761/jellytoast/Main.java
@@ -7,25 +7,27 @@ import android.graphics.drawable.Drawable;
 import android.util.TypedValue;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import de.robv.android.xposed.IXposedHookZygoteInit;
-import de.robv.android.xposed.callbacks.XC_LayoutInflated;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 
 public class Main implements IXposedHookZygoteInit{
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
         XModuleResources modRes = XModuleResources.createInstance(startupParam.modulePath, (XResources)XResources.getSystem());
         final Drawable toastBackground = modRes.getDrawable(R.drawable.toast_bg_jellybean);
-        XResources.hookSystemWideLayout("android", "layout", "transient_notification", new XC_LayoutInflated() {
-
-            @Override
-            public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-                TextView toast = (TextView) liparam.view.findViewById(android.R.id.message);
-                toast.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
-                toast.setTypeface(Typeface.DEFAULT);
-                LinearLayout toastLayout = (LinearLayout) liparam.view;
-                toastLayout.setBackground(toastBackground);
-            }
-
-        });
+        XposedHelpers.findAndHookMethod(Toast.class, "show", new XC_MethodHook() {
+        	@Override
+        	protected void beforeHookedMethod(MethodHookParam param)
+        			throws Throwable {
+        		Toast toast = (Toast) param.thisObject;
+        		LinearLayout layout = (LinearLayout) toast.getView();
+        		layout.setBackground(toastBackground);
+        		TextView message = (TextView) layout.findViewById(android.R.id.message);
+        		message.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+                message.setTypeface(Typeface.DEFAULT);
+        	}
+		});
     }
 }
